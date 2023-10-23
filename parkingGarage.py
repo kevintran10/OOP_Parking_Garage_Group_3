5d7c12b7d6a25e7c0a1ea1b779f4144578a3638a
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2023-10-23 14:39:47 -0400

    added new function to count spaces and edited grammer and imporved exiting function loops

--- parkingGarage.py
+++ parkingGarage.py
@@ -7,10 +7,12 @@ class ParkingGarage():
 
     def runner(self):
         while True:
-            choose = input('What would you like to do? (take ticket, pay, leave, quit) ').lower()
+            choose = input('What would you like to do? Show availability, take ticket, pay ticket, leave garage or quit.: ').lower()
             if choose == 'quit':
                 print('Have a nice day! Please exit the garage to the left. ')
                 break
+            elif choose == 'show':
+                self.showSpaces()
             elif choose == 'take':
                 self.takeTicket() 
             elif choose == 'pay':
@@ -18,55 +20,64 @@ class ParkingGarage():
             elif choose == 'leave':
                 self.leaveGarage()
             else:
-                print('Invalid entry, please make a choice.')
-                
-
-    def takeTicket(self):
-        tic_booth = int(input('Please press 1 to take ticket. '))
+                print('Invalid entry, please make another choice.')
+    
+    def showSpaces(self):
+        open_spaces = int(input('Press 1 to see how may open spaces.: '))
 
         while True:
-            if tic_booth == 1:
-                print(f'Please pull forward to park')
+            if open_spaces == 1:
+                print(f'There are {self.parkingSpaces} spaces open')
+                break
+            if open_spaces != 1:
+                open_spaces = int(input('Invalid entry please enter 1 to see available spaces.: '))
+
+
+    def takeTicket(self):
+        tic_booth = int(input('Please press 2 to take ticket.: '))
+       
+        while True:
+            if tic_booth == 2:
+                print(f'Please pull forward to park.')
                 self.tickets -= 1
                 self.parkingSpaces -= 1
-                print(f'There are {self.parkingSpaces} open spaces left.')    
-            else:
-                print(f'Invalid entry, please press 1 for a ticket')
-            break
-
-
-
+                print(f'There are {self.parkingSpaces} open spaces left.')  
+                break  
+            if tic_booth > 2:
+                tic_booth = int(input('Invalid entry, please press 1 to take ticket.: '))
 
     
     def payForParking(self):
-        pay_now = int(input('Please enter $15 to pay for your day parking '))
+        pay_now = int(input('Please enter 15 to pay for your day parking ticket.: '))
        
         while True:
             if pay_now == 15:
-                print('Thank you for paying your ticket, you have 15 minutes to exit the garage.')
+                print("Thank you for paying your ticket, you have 15 minutes to exit the garage. Select 'leave' to leave garage.")
                 self.currentTicket = True
                 break
             elif pay_now < 15:
-                pay_now = int(input('Invalid entry please pay for your parking. Please enter $15 to pay for your day parking. '))
-            else:
-                break
-
-
+                pay_now = int(input('Invalid entry please pay for your parking. Please enter 15 to pay for your day parking.: '))
+           
 
     def leaveGarage(self):
-        pay_status = int(input('Please enter $15 to pay for your day parking '))
+        pay_status = int(input('Please enter 3 to leave, if parking is not paid, enter 15 to pay for your day parking.: '))
         
         while True:
-            if pay_status == 15:
-                print('Thank you for paying your ticket, you have 15 minutes to exit the garage.')
-                self.currentTicket = False
+            if pay_status == 3:
+                print('Thank you have a nice day!')
+                self.currentTicket == True
+                self.tickets += 1
+                self.parkingSpaces += 1
+                print(f'There are {self.parkingSpaces} available spaces.')
                 break
-            elif pay_now < 15:
-                pay_now = int(input('Invalid entry please pay for your parking. Please enter $15 to pay for your day parking. '))
-
-        self.tickets += 1
-        self.parkingSpaces += 1
-        print(f'There are {self.parkingSpaces} available.')
+            elif self.currentTicket == False:
+                break
+            elif pay_status == 15:
+                self.currentTicket == False
+                print('Please enter 15 to pay for your parking ticket.: ')
+               
+    
+            
  
 Oop = ParkingGarage() 
 Oop.runner()
